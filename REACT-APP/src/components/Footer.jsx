1219544b580edd0971830c23991579e079e2db44
unknown <Fayaz khan>
2024-05-12 11:21:14 +0500

    footer created

---
+++ REACT-APP/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <span className="navbar-text">
+            Created by ALFA : ISO 9001-2024
+          </span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Footer;
