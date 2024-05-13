6d03313cb93eac257f87005322d643bdacc78c75
unknown <Fayaz khan>
2024-05-13 19:01:58 +0500

    set data in Local Storage in the form of string

--- REACT-APP/src/App.jsx
+++ REACT-APP/src/App.jsx
@@ -1,37 +1,44 @@
+import { useEffect } from "react"
 import Home from "./pages/Home"
 
 function App() {
   
-  let products = [
-    {
-      title:'product-1',
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-      Image:'/images/burger.png',
-      price: "Price : " + 300 
-    },
-    {
-      title:'product-2',
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-      Image:'/images/burger2.png',
-      price: "Price : " + 760 
-    },
-    {
-      title:'product-3',
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-      Image:'/images/burger.png',
-      price: "Price : " + 1300 
-    },
-    {
-      title:'product-4',
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-      Image:'/images/burger2.png',
-      price: "Price : " + 800 
-    }
-  ]
+ 
+  useEffect(()=>{
+    window.localStorage.setItem('Products', JSON.stringify([
+      {
+        title:'product-1',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        Image:'/images/burger.png',
+        price: "Price : " + 300 
+      },
+      {
+        title:'product-2',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        Image:'/images/burger2.png',
+        price: "Price : " + 760 
+      },
+      {
+        title:'product-3',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        Image:'/images/burger.png',
+        price: "Price : " + 1300 
+      },
+      {
+        title:'product-4',
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        Image:'/images/burger2.png',
+        price: "Price : " + 800 
+      }
+    ])  );
+   }, [])
+
 
   return (
     <>
-     <Home products = {products} />
+     {/* <Home products = {products} /> */}
+
+     {/* <button onClick={handleClick}>click</button> */}
     </>
   )
 }
