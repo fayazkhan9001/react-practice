7d209866c458f80c11625dd8318af1003a748d8b
unknown <Fayaz khan>
2024-05-13 19:14:47 +0500

    get all item from local storage

--- REACT-APP/src/App.jsx
+++ REACT-APP/src/App.jsx
@@ -1,42 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Home from "./pages/Home"
 
 function App() {
+  let [products, setProducts] = useState([]);
+
+useEffect(()=>{
+  let prods = JSON.parse(window.localStorage.getItem("Products"));
+   setProducts( prods);
   
- 
-  useEffect(()=>{
-    window.localStorage.setItem('Products', JSON.stringify([
-      {
-        title:'product-1',
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-        Image:'/images/burger.png',
-        price: "Price : " + 300 
-      },
-      {
-        title:'product-2',
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-        Image:'/images/burger2.png',
-        price: "Price : " + 760 
-      },
-      {
-        title:'product-3',
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-        Image:'/images/burger.png',
-        price: "Price : " + 1300 
-      },
-      {
-        title:'product-4',
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
-        Image:'/images/burger2.png',
-        price: "Price : " + 800 
-      }
-    ])  );
-   }, [])
+}, [])
 
 
   return (
     <>
-     {/* <Home products = {products} /> */}
+     <Home products = {products} />
 
      {/* <button onClick={handleClick}>click</button> */}
     </>
